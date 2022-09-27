cc6ed5b9eee97e29fd8583307df5af3ad20bd6a0
reminat <67228469+reminat@users.noreply.github.com>
2022-09-27 22:24:20 +0200

    Setup fonctionnel

--- pzem/src/main.cpp
+++ pzem/src/main.cpp
@@ -1,9 +1,89 @@
-#include <Arduino.h>
+/*
+Copyright (c) 2021 Jakub Mandula
 
-void setup() {
-  // put your setup code here, to run once:
+Example of using one PZEM module with Software Serial interface.
+================================================================
+
+If only RX and TX pins are passed to the constructor, software
+serial interface will be used for communication with the module.
+
+*/
+
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+PZEM004Tv30 pzem(pzemSWSerial);
+
+void setup()
+{
+  /* Debugging serial */
+  Serial.begin(115200);
+  pzem.resetEnergy();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+  //  Serial.print("Custom Address:");
+  //  Serial.println(pzem.readAddress(), HEX);
+
+  // Read the data from the sensor
+  float voltage = pzem.voltage();
+  float current = pzem.current();
+  float power = pzem.power();
+  float energy = pzem.energy();
+  float frequency = pzem.frequency();
+  float pf = pzem.pf();
+
+  // Check if the data is valid
+  if (isnan(voltage))
+  {
+    Serial.println("Error reading voltage");
+  }
+  else if (isnan(current))
+  {
+    Serial.println("Error reading current");
+  }
+  else if (isnan(power))
+  {
+    Serial.println("Error reading power");
+  }
+  else if (isnan(energy))
+  {
+    Serial.println("Error reading energy");
+  }
+  else if (isnan(frequency))
+  {
+    Serial.println("Error reading frequency");
+  }
+  else if (isnan(pf))
+  {
+    Serial.println("Error reading power factor");
+  }
+  else
+  {
+
+    // Print the values to the Serial console
+    Serial.print("Voltage: ");
+    Serial.print(voltage);
+    Serial.println("V");
+    Serial.print("Current: ");
+    Serial.print(current);
+    Serial.println("A");
+    Serial.print("Power: ");
+    Serial.print(power);
+    Serial.println("W");
+    Serial.print("Energy: ");
+    Serial.print(energy, 3);
+    Serial.println("kWh");
+    Serial.print("Frequency: ");
+    Serial.print(frequency, 1);
+    Serial.println("Hz");
+    Serial.print("PF: ");
+    Serial.println(pf);
+  }
+
+  Serial.println();
+  delay(2000);
 }
